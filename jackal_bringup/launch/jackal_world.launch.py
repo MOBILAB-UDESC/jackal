13e505b7762563761e389b52e0d0f8b711f21938
NiltonSV <incripcion18@gmail.com>
2026-02-09 12:36:31 -0300

    remove .sdf extension from world_name arg

--- jackal_bringup/launch/jackal_world.launch.py
+++ jackal_bringup/launch/jackal_world.launch.py
@@ -17,9 +17,9 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     """Launch Gazebo world and bridge clock topic."""
-    bringup_pkf_path = get_package_share_directory("jackal_bringup")
+    bringup_pkf_path = get_package_share_directory('jackal_bringup')
 
-    ament_prefix_path = os.getenv("AMENT_PREFIX_PATH", "")
+    ament_prefix_path = os.getenv('AMENT_PREFIX_PATH', '')
     packages_paths = [os.path.join(p, 'share') for p in ament_prefix_path.split(':')]
 
     gz_sim_resource_path = SetEnvironmentVariable(
@@ -27,12 +27,12 @@ def generate_launch_description():
         value=[
             ':'.join(packages_paths),
             ':',
-            os.getenv('GZ_SIM_RESOURCE_PATH', "")
+            os.getenv('GZ_SIM_RESOURCE_PATH', '')
         ]
     )
 
-    gazebo_path = PathJoinSubstitution(
-        [bringup_pkf_path, 'worlds', LaunchConfiguration('world_name')])
+    gazebo_world_path = PathJoinSubstitution(
+        [bringup_pkf_path, 'worlds', [LaunchConfiguration('world_name'), '.sdf']])
 
     gazebo_world_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -44,7 +44,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             'render_engine': 'ogre2',
-            'gz_args': ['-r -v 1 ', gazebo_path],
+            'gz_args': ['-r -v 1 ', gazebo_world_path],
             'on_exit_shutdown': 'true'
         }.items(),
     )
@@ -60,11 +60,11 @@ def generate_launch_description():
     args = [
         DeclareLaunchArgument(
             'world_name',
-            default_value='empty.sdf',
+            default_value='empty',
             choices=[
-                'empty.sdf',
-                'mobilab.sdf',
-                'playground.sdf'
+                'empty',
+                'mobilab',
+                'playground'
             ],
             description='Gazebo world file (custom worlds should be added to the "worlds" folder of this package'
         ),
