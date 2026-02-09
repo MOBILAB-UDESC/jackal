4b56041c8d77505a650b1511c67013326663483f
NiltonSV <incripcion18@gmail.com>
2026-02-09 16:47:34 -0300

    add sensors executable

--- jackal_bringup/scripts/jackal_sensors.sh
+++ jackal_bringup/scripts/jackal_sensors.sh
@@ -4,5 +4,4 @@ export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 cd ${HOME}/mobi_robots
 source install/setup.bash
 
-ros2 launch jackal_bringup jackal_mcu_setup.launch.py namespace:=j100_0929
-
+ros2 launch jackal_bringup jackal_sensors.launch.py
--- jackal_bringup/scripts/mcu_diagnostics.sh
+++ jackal_bringup/scripts/mcu_diagnostics.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export ROS_DOMAIN_ID=7
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 cd ${HOME}/mobi_robots
 source install/setup.bash
 
-ros2 launch jackal_description jackal_diagnostic.launch.py namespace:=j100_0929
+ros2 launch jackal_bringup jackal_diagnostic.launch.py namespace:=j100_0929
